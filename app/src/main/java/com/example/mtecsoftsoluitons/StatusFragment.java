f0dcfddcc270ad70c2e5a2d9d0e6eb9e8feb8604
DESKTOP-58EJ12O\Hafiz Naeem Hassan <naeem.alpha2labs1@gmail.com>
2019-05-24 15:54:58 +0500

    string file changes

--- app/src/main/java/com/example/mtecsoftsoluitons/StatusFragment.java
+++ app/src/main/java/com/example/mtecsoftsoluitons/StatusFragment.java
@@ -1,6 +1,7 @@
 package com.example.mtecsoftsoluitons;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,15 +16,20 @@ public class StatusFragment extends Fragment {
 
 
     public StatusFragment() {
+        sendData();
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_status, container, false);
+    }
+
+    private void sendData() {
+        Intent i = new Intent();
+        i.putExtra("key", "Hafiz Naeem Hassan");
+        startActivity(i);
     }
 
 }
